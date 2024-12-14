853aebc83df1a5a81d8cea67176217e88724eeb6
Ebeguy Yapo <91952616+uvsq22103869@users.noreply.github.com>
2024-12-14 20:15:09 +0100

    Ajout de la table Film

--- main.sql
+++ main.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE FILM (
+    id_Film INT PRIMARY KEY,          
+    Titre VARCHAR(255) NOT NULL,     
+    Date_sortie DATE NOT NULL,        
+    Durée INT NOT NULL,                -- en minutes
+    Audience VARCHAR(50) NOT NULL,    -- Public visé
+    Temps_exploitation INT DEFAULT 15  -- Temps par défaut en jours
+);
