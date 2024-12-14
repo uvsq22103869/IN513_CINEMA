4533a4da74e6e7c9a05998e9b4e37d1512f2089a
Ebeguy Yapo <91952616+uvsq22103869@users.noreply.github.com>
2024-12-14 20:18:55 +0100

    Table Genre + Film_Genre

--- main.sql
+++ main.sql
@@ -6,3 +6,16 @@ CREATE TABLE FILM (
     Audience VARCHAR(50) NOT NULL,    -- Public visé
     Temps_exploitation INT DEFAULT 15  -- Temps par défaut en jours
 );
+
+CREATE TABLE GENRE (
+    id_Genre INT PRIMARY KEY,  
+    Genre VARCHAR(50) NOT NULL   
+);
+
+CREATE TABLE FILM_GENRE (
+    id_Film INT NOT NULL,
+    id_Genre INT NOT NULL,
+    PRIMARY KEY (id_Film, id_Genre),
+    FOREIGN KEY (id_Film) REFERENCES FILM(id_Film) ON DELETE CASCADE,
+    FOREIGN KEY (id_Genre) REFERENCES GENRE(id_Genre) ON DELETE CASCADE
+);
