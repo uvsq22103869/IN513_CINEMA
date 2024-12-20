2eb2afff09759267b6af373cbe1980aee9f365e9
Ebeguy Yapo <91952616+uvsq22103869@users.noreply.github.com>
2024-12-20 13:05:53 +0100

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -13,3 +13,27 @@ BEGIN
     END IF;
 END;
 /
+
+-- Vérification du chevauchement des horaires des séances 
+CREATE OR REPLACE TRIGGER trg_verifier_horaires_seance
+BEFORE INSERT OR UPDATE ON SEANCE
+FOR EACH ROW
+DECLARE
+    conflit_count NUMBER;
+    delai_min NUMBER := 15; 
+BEGIN
+    SELECT COUNT(*)
+    INTO conflit_count
+    FROM SEANCE
+    WHERE id_Salle = :NEW.id_Salle
+      AND Date_Seance = :NEW.Date_Seance
+      AND (   (:NEW.Heure_Début BETWEEN Heure_Début - INTERVAL '15' MINUTE AND Heure_Fin + INTERVAL '15' MINUTE)
+           OR (:NEW.Heure_Fin BETWEEN Heure_Début - INTERVAL '15' MINUTE AND Heure_Fin + INTERVAL '15' MINUTE)
+           OR (:NEW.Heure_Début <= Heure_Début AND :NEW.Heure_Fin >= Heure_Fin));
+
+    IF conflit_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Conflit d’horaires : une autre séance est prévue trop proche dans cette salle à cette date.');
+    END IF;
+END;
+/
+
